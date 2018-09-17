bc30bde6db7e070d84808b56b98cce376d27ff54
vagrant <zfolwic@rei.com>
2018-09-17 15:29:09 -0700

    spa

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxt-demo
       </h1>
       <h2 class="subtitle">
-        My horrifying Nuxt.js project
+        My horrifying client-rendered Nuxt.js project
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
